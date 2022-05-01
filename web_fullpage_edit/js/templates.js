446dc234939d424a77f24ab2777fa4318d844e01
yuxiaotu <yuxiaotu1024@qq.com>
2022-05-01 10:23:01 +0800

    feat: fullpage edit

--- web_fullpage_edit/js/templates.js
+++ web_fullpage_edit/js/templates.js
@@ -6,7 +6,8 @@ const templates = [
         type: "div",
         id: "t0_1",
         styleSheet: {
-          background: "#ccc",
+          position: "relative",
+          "background-image": "linear-gradient( 135deg, #97ABFF 10%, #123597 100%)"
         },
         className: [],
         children: [
@@ -14,15 +15,23 @@ const templates = [
             id: "t0_1_1",
             type: "div",
             styleSheet: {
-              background: "#fff",
-              width: "30%",
-              height: "30%",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              width: "50px",
+              height: "50px",
+              transform: "translate(-50%, -50%)",
+              background: "rgba( 255, 255, 255, 0.2 )",
+              "box-shadow": "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+              "backdrop-filter": "blur( 15px )",
+              "-webkit-backdrop-filter": "blur( 15px )",
+              "border-radius": "4px",
+              "border": "1px solid rgba( 255, 255, 255, 0.18 )",
             },
             className: [],
-            content: "Hello",
           },
         ],
-      },
+      }
     ],
   },
   {
