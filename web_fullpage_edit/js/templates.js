209dd2005103203ce8a98d7c40174186960b79fd
yuxiaotu <yuxiaotu1024@qq.com>
2022-05-02 16:19:26 +0800

    feat: edit

--- web_fullpage_edit/js/templates.js
+++ web_fullpage_edit/js/templates.js
@@ -18,8 +18,8 @@ const templates = [
               position: "absolute",
               top: "50%",
               left: "50%",
-              width: "50px",
-              height: "50px",
+              width: "50%",
+              height: "50%",
               transform: "translate(-50%, -50%)",
               background: "rgba( 255, 255, 255, 0.2 )",
               "box-shadow": "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
@@ -27,9 +27,38 @@ const templates = [
               "-webkit-backdrop-filter": "blur( 15px )",
               "border-radius": "4px",
               "border": "1px solid rgba( 255, 255, 255, 0.18 )",
+              "z-index": 2
             },
-            className: [],
+            className: []
           },
+          {
+            id: "t0_1_2",
+            type: "div",
+            styleSheet: {
+              position: "absolute",
+              top: "16%",
+              left: "16%",
+              width: "18%",
+              height: "25%",
+              background: "#0aa1dd",
+              "border-radius": "50%",
+              "z-index": 1
+            }
+          },
+          {
+            id: "t0_1_3",
+            type: "div",
+            styleSheet: {
+              position: "absolute",
+              bottom: "16%",
+              right: "16%",
+              width: "18%",
+              height: "25%",
+              background: "#79DAE8",
+              "border-radius": "50%",
+              "z-index": 1
+            }
+          }
         ],
       }
     ],
@@ -41,10 +70,32 @@ const templates = [
         type: "div",
         id: "t1_1",
         styleSheet: {
-          background: "blue",
+          display: "flex",
         },
         className: [],
-        content: "",
+        children: [
+          {
+            id: "t1_1_1",
+            type: "div",
+            styleSheet: {
+              width: "40%",
+              height: "100%",
+              "background-image": "url(https://cdn.pixabay.com/photo/2021/11/18/21/58/sunflower-6807491_960_720.jpg)",
+              "background-size": "cover"
+            },
+            className: []
+          },
+          {
+            id: "t1_1_2",
+            type: "div",
+            styleSheet: {
+              width: "60%",
+              height: "100%",
+              background: "#B9F8D3"
+            },
+            className: []
+          }
+        ]
       },
     ],
   },
